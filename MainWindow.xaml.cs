291faea851b3cccd62d725ebee2c2b930aae02a3
Thorgardd <rouviller.jeremy59@gmail.com>
2021-11-03 16:34:36 +0100

    First Upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Windows;
+
+namespace JustPrice
+{
+    public partial class MainWindow : Window
+    {
+        
+
+        private void Verification()
+        {
+            
+
+            if (UserNumber(Int32.Parse(UserBoxNumber.Text)) < numberWin)
+            {
+                Answer.Text = "C'est plus grand";
+                return;
+            }
+            else if (UserNumber(Int32.Parse(UserBoxNumber.Text)) > numberWin)
+            {
+                Answer.Text = "C'est plus petit";
+                return;
+            }
+            else if (UserNumber(Int32.Parse(UserBoxNumber.Text)) == numberWin)
+            {
+                Answer.Text = "VOUS AVEZ REUSSI";
+                return;
+            }
+            else
+            {
+                Answer.Text = "ERREUR";
+            }
+
+        }
+
+        private void Submit_Click(object sender, RoutedEventArgs e)
+        {
+            NumberWin.Content = numberWin;
+            Verification();
+
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private static readonly Random rnd = new Random();
+
+        public static int RandomNumber()
+        {
+            return rnd.Next(1, 50);
+        }
+
+        private int numberWin = RandomNumber();
+
+
+        public int UserNumber(int nbUser)
+        {
+            int nbUserBox;
+            nbUserBox = Int32.Parse(UserBoxNumber.Text);
+            return nbUserBox;
+        }
+    }
+}
